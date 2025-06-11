953bfc621d29d09f26ca25ffe33a632880c5e596
GerardoExe <gerardodonelli1@gmail.com>
2025-06-11 11:31:40 -0300

    Rediseño de la solución y estructura de los proyectos (Servidor y Cliente).

---
+++ TP/61271 - Donelli, Gerardo Exequiel/tp6/servidor/Models/Models.cs
@@ -0,0 +1,16 @@
+namespace servidor.Models;
+
+public class Producto
+{
+    // Se implementará en el siguiente commit
+}
+
+public class Compra 
+{
+    // Se implementará en el siguiente commit
+}
+
+public class ItemCompra
+{
+    // Se implementará en el siguiente commit
+}
